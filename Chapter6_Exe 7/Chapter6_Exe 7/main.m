fa6cd0c53596a6f5fa2beb8505ab034cf72ce820
jasonchn <jasonchn@outlook.com>
2013-06-08 17:15:50 +0800

    New

---
+++ Chapter6_Exe 7/Chapter6_Exe 7/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Chapter6_Exe 7
+//
+//  Created by Jason Zhao on 6/8/13.
+//  Copyright (c) 2013 Jason Zhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
